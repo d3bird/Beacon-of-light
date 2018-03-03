d1629059c9f4f97c9fc70193b977373535899aa5
Scott Crawford <dogbird9@gmail.com>
2018-03-03 15:12:23 -0500

    first base commit scott

---
+++ app/app/src/main/java/e/dogbi/beacon/MainActivity.java
@@ -0,0 +1,29 @@
+package e.dogbi.beacon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendfirebase(View view) {
+
+    }
+
+    public void recivefirebase(View view) {
+
+    }
+
+    public void changetext(){
+
+
+    }
+
+
+}
