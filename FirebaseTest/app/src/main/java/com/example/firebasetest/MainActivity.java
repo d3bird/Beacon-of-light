d0b124275c2ef503d99c6bc732ee75137f45bc6f
Jordan Ball <jmball2016@gmail.com>
2018-03-04 04:25:14 -0500

    Jordan Added child event listener

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -23,8 +23,12 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -62,7 +66,35 @@ public class MainActivity extends AppCompatActivity {
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Priority1");
 
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Entry myentry = dataSnapshot.getValue(Entry.class);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        })
     }
 
     public void onItemSelected(AdapterView<?> parent, View view,
--- sever/Functions/functions/index.js
+++ sever/Functions/functions/index.js
@@ -6,3 +6,23 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+// Import Admin SDK
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.database();
+var ref = db.ref("Priority0");
+
+exports.sendAlert = functions.ref.onWrite(event => {
+    var newPost = event.data.val();
+    if(newPost.status === 3){ //check to see if the person pushed an emergency
+        var postsRef = ref.child("Priority0"); //send a new entry with an alert code
+        var newPostRef = postsRef.push();
+        newPostRef.set({
+            ID: newPost.ID,
+            GPS: newPost.GPS,
+            status: 5
+        });
+    }
+    return event.data
+  });
