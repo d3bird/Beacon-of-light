4a89d5c8f4b0ae535cb710e041dd469402f7820a
Scott Crawford <dogbird9@gmail.com>
2018-03-04 01:30:22 -0500

    created a map screen scott

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.firebasetest;
 
+import android.*;
+import android.Manifest;
+import android.app.VoiceInteractor;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,16 +26,18 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    String ID ="";
-    String GPS ="";
-    int status =0;
-    int priority =0;
+    String ID = "";
+    String GPS = "";
+    int status = 0;
+    int priority = 0;
 
-  public  EditText user; //= findViewById(R.id.name);
-  public  EditText userstatus;// = findViewById(R.id.personstatus);
+    public EditText user; //= findViewById(R.id.name);
+    public EditText userstatus;// = findViewById(R.id.personstatus);
+    public TextView gpsstring;
 
-       private LocationManager locationManager;
-       private LocationListener locationListener;
+    public Button gpsbutton;// = findViewById(r.id.GPSbutton);
+
+
 
 
     @Override
@@ -33,55 +45,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         user = findViewById(R.id.name);
-         userstatus = findViewById(R.id.personstatus);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        user = findViewById(R.id.name);
+        userstatus = findViewById(R.id.personstatus);
 
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                //t.append("\n " + location.getLongitude() + " " + location.getLatitude());
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
-            }
-        };
-
-        //locationManager.requestLocationUpdates("");
+        gpsstring = findViewById(R.id.GPStext);
+        gpsbutton = findViewById(R.id.GPSbutton);
     }
 
 
+// locationManager.requestLocationUpdates("gps", 60000, 0, locationListener);
 
     public void sendinfo(View view) {//sends to firebase
-        //ID = user.getText().toString();
-        //status = userstatus.getText().to
-        Entry toast = new Entry(ID, GPS,status);
+        ID = user.getText().toString();
+        status =Integer.valueOf(userstatus.getText().toString());
+        Entry waffles = new Entry(ID, GPS,status);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Priority"+priority);//determins were it is sent to
 
-        myRef.push().setValue(toast);//sends the entry
+        myRef.push().setValue(waffles);//sends the entry
 
     }
 
     public void getGPS(View view) {
-
+       // gpsstring.setText (location.getLongitude() + " " + location.getLatitude());
 
     }
 
 
+    public void switchtomap(View view) {
+        startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+    }
 }
