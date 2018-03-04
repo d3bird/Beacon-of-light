b8fe3096587e17bc2efe1534b09250cf333aa667
Scott Crawford <dogbird9@gmail.com>
2018-03-04 04:04:31 -0500

    added drop down menue scott

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -16,8 +16,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -48,11 +51,29 @@ public class MainActivity extends AppCompatActivity {
         user = findViewById(R.id.name);
         userstatus = findViewById(R.id.personstatus);
 
-        gpsstring = findViewById(R.id.GPStext);
+      //  gpsstring = findViewById(R.id.GPStext);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.options, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
 
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 
 
     public void sendinfo(View view) {//sends to firebase
@@ -75,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
     public void receive(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Priority1");
-        //myRef.
+
     }
 
 
