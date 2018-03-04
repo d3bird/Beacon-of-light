b3a4fc990e0e72b145fa61bbcaa7f82b1dd2e927
Scott Crawford <dogbird9@gmail.com>
2018-03-03 20:34:50 -0500

    added debug buttons scott

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -9,13 +10,18 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    String ID;
+    String GPS;
+    int status;
+    int priority;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
+   /* @Override
     protected void onStart() {
         super.onStart();
         Entry myentry = new Entry("scott2", "nutterbutters", 8);
@@ -24,9 +30,16 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("Priority0");
 
         myRef.push().setValue(myentry);
+    }*/
+
+
+    public void sendinfo(View view) {
+        Entry myentry = new Entry(ID, GPS, status);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Priority"+priority);
+
+        myRef.push().setValue(myentry);
 
     }
-
-
-
 }
